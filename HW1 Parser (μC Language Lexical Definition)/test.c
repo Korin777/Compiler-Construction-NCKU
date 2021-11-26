55edfc7baaf13faeeeba57bbf39a4402a87988b0
Korin777 <57750932+Korin777@users.noreply.github.com>
2021-11-26 11:48:04 +0800

    Add files via upload

---
+++ HW1 Parser (μC Language Lexical Definition)/test.c
@@ -0,0 +1,12 @@
+// c program to demonstrate
+// example of strupr() function.
+#include<stdio.h>
+#include<string.h>
+  
+int main()
+{
+    char str[ ] = "geeksforgeeks is the best";
+    //converting the given string into uppercase.
+    printf("%s\n", strupr (str));
+    return  0;
+}
